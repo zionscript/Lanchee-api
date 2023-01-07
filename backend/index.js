22d00b74eeb96f64f1fb6bd815b71a78d28ab800
yokatsu <wassabi1002@gmail.com>
2023-01-07 10:45:22 -0300

    firestore added

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const { db } = require('./firebase.js');
 const dotenv = require('dotenv');
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
 dotenv.config();
 const app = express();
 
-app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended:true}))
 
-app.get('/',(req,res)=>{
-    res.status(200).json({"chave":"SouFoda"});
+app.get('/', async(req,res)=>{
+    const restaurantRef = db.collection('restaurantsData').doc('g2xiR2JyF4mGLOw3y4KC')
+    const doc = await restaurantRef.get();
+    // if (!doc.exists) {
+    //     return res.sendStatus(400)
+    // }
+
+    res.status(200).send(doc.data())
 });
 
 const port = process.env.PORT;
